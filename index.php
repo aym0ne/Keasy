eb73c96d3d8e01a120f78e03f57cac7189666459
aym0ne <81880627+aym0ne@users.noreply.github.com>
2021-04-03 16:44:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
+<html>
+ 
+<head>
 
+</head>
+<link rel="stylesheet"> 
+
+<body>
+<div id="top">
+<h1 style = "color:blue;"
+>Prototype 2</h1>
+
+</div>
+<?php
+include "db_connect.php";
+?>
+Tu es un admin:
+<a href="mainpageadmin.php">Admin</a>
+<br>
+<br>
+Demande de cle:
+<a href="askkey.php">Faire une demande</a>
+<br>
+ <br>
+
+<?php
+?>
+
+<hr>
+<br>
+You are an admin:
+<a href="ENmainpageadmin.php">Admin</a>
+<br>
+<br>
+Ask for a key:
+<a href="ENaskkey.php">Key request</a>
+<br>
+
+
+</body>
+
+</html>
