dd1275203ece1f8d60adbab0c21a64bdcd3d066d
JonathanBaker15 <81880591+JonathanBaker15@users.noreply.github.com>
2021-04-03 16:47:09 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
+</html>
+
+
+
+<html>
+
+<head>
+
+</head>
+
+<h1 style = "color:blue;"
+>Prototype 3</h1>
+
+</div>
+<?php
+include "db_connect.php";
+?>
+Login:
+<a href="mainpagelogin.php">Admin</a>
+<br>
+<br>
+Demande de cle:
+<a href="askkey.php">Faire une demande</a>
+<br>
+ <br>
+<?php
+?>
+
+
+</body>
+
+</html>
+
