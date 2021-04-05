0cc0d882583656ed1d8df70eb1886897ce145e6b
Imaniuwu <81880796+Imaniuwu@users.noreply.github.com>
2021-04-05 16:23:42 -0400

    Update logincvg.php

--- logincvg.php
+++ logincvg.php
@@ -108,7 +108,7 @@ main grid */
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>Login pour CVG</title>
   <link rel="stylesheet" href="login-page.css">
 </head>
 
