e910c39963aca373bd1cbec04ecc37a4b1dfa2ab
Imaniuwu <81880796+Imaniuwu@users.noreply.github.com>
2021-04-06 18:27:59 -0400

    Update transferchg.php

--- transferchg.php
+++ transferchg.php
@@ -47,7 +47,7 @@ function googleTranslateElementInit() {
 <?php
 include "db_connect.php";
 $name= $_GET["nom"];
-$sql = "INSERT INTO chg(Nom,Prenom,Faculte,Departement,Nombre_de_cle,Code_de_cle,Nom_piece,Batiment,Date_de_remise,Date_de_fin,Depot) SELECT Nom,Prenom,Faculte,Departement,Nombre_de_cle,Code_de_cle,Nom_piece,Batiment,Date_de_remise,Date_de_fin,Depot FROM chg WHERE  Nom  LIKE'%" .$name. "%' ";
+$sql = "INSERT INTO chg(Nom,Prenom,Faculte,Departement,Nombre_de_cle,Code_de_cle,Nom_piece,Batiment,Date_de_remise,Date_de_fin,Depot) SELECT Nom,Prenom,Faculte,Departement,Nombre_de_cle,Code_de_cle,Nom_piece,Batiment,Date_de_remise,Date_de_fin,Depot FROM general WHERE  Nom  LIKE'%" .$name. "%' ";
 $result = $mysqli->query($sql);
 if ($mysqli -> error) {
   echo "<h1>Il y a eu une erreur. Veuillez réessayer.</h1> " ;
