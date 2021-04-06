eef6b0a2a9254fa5291cec8be82b544f1267da6f
Imaniuwu <81880796+Imaniuwu@users.noreply.github.com>
2021-04-06 00:18:49 -0400

    Update addfaculte.php

--- addfaculte.php
+++ addfaculte.php
@@ -1,3 +1,42 @@
+<meta charset="UTF-8">
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+<!DOCTYPE html>
+<html lang="fr">
+<style>
+  *{
+    margin: 0;
+    paading: 0;
+    font-family:Arial, Helvetica, sans-serif;
+  }body{
+    background:#f2f2f2;
+  }h1{
+  text-align: center;
+  }.link{
+  text-align: center;
+  }.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color:#2d2d2c;
+   color: white;
+   text-align: center;
+ }.square {
+  width: 50px;
+  height: 280px;
+  background: #f2f2f2;
+}  
+/* LARGE SCREENS */
+		@media(min-width:900px) {
+  .square {
+  width: 300px;
+  height: 280px;
+  background: #f2f2f2;
+  }   			
+}
+</style>
+<title>Add</title>
+</html>
 <?php
 
 include "db_connect.php";
@@ -15,7 +54,7 @@ $new_depot = $_GET["newdepot"];
 
 
 // Search the database for the word bike
-echo "<h2>Vous avez ajoutez quelqu'un</h2>";
+echo "<h1>Vous avez ajouté quelqu'un à la liste</h1>";
 
 $sql = "INSERT INTO faculte (ID, Nom, Prenom, Departement, Nombre_de_cle, Code_de_cle, Nom_piece, Batiment, Date_de_remise, Date_de_fin, Depot) VALUES (NULL, '$new_nom', '$new_prenom', '$new_departement', '$new_nombre_de_cle', '$new_code_de_cle', '$new_nom_piece', '$new_batiment', '$new_date_de_remise', '$new_date_de_fin', '$new_depot')";
 $result = $mysqli->query($sql);
@@ -23,5 +62,15 @@ $result = $mysqli->query($sql);
 //include "search.php";
 
 ?>
-
-<a href="mainpagefaculte.php">Retour</a>
+<html>
+<body>
+<h1>Vous avez ajouté quelqu'un à la liste!</h1>
+<div class = "square"></div>
+<div class = "link">
+      <a href="mainpagefaculte.php"><span>Retourner à la page d'accueil</span></a>
+      </div>
+<div class="footer">
+<p>Développé par Keasy</p>
+</div>
+</body>
+</html>
