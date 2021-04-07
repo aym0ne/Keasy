d125c135acbb56c18edd96019433457870366349
JonathanBaker15 <81880591+JonathanBaker15@users.noreply.github.com>
2021-04-07 12:19:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,49 @@
 <!DOCTYPE html>
 <html lang="fr">
 <div id="google_translate_element"></div>
-
 <script type="text/javascript">
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
 </script>
-
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-
 </html>
-
-
-
 <html>
-
 <head>
-
+<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
-
-<h1 style = "color:blue;"
->Prototype 3</h1>
-
+ 
+<div class="header">
+    <h1>GESTION DE CLÉS</h1>
+    <p> Bienvenue sur le site de demande et de gestion de clés de l'Université d'Ottawa</p>
+  </div>
+ 
 </div>
 <?php
 include "db_connect.php";
 ?>
-Login:
-<a href="mainpagelogin.php">Admin</a>
+<div class="row">
+    <div class="column">
+      <h2>OPTION DEMANDEUR DE CLÉ
+        <p>Appuyez içi pour avoir accès au formulaire de demande de clé et faire votre demande.</p>
+      </h2>
+      <a href="mainpagelogin.php" class="button"><span>S'inscrire</span></a>
+</div>
+ 
+<div class="column">
+<h2>OPTION ADMINISTRATEUR</h2>
+<p>Appuyez içi pour vous connecter à votre compte pour accéder à l'information.
+</h2>
 <br>
 <br>
-Demande de cle:
-<a href="askkey.php">Faire une demande</a>
 <br>
- <br>
+<a href="askkey.php" class="button"><span>Se connecter</span></a>
+</div>
+<div class="vl"></div>
+<div class="footer">
+<p>Développé par Keasy</p>
 <?php
 ?>
-
-
+</div>
 </body>
-
 </html>
-
