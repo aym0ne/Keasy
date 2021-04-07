79df964ea184ad4d660919d2f0a14a30821f4bb9
JonathanBaker15 <81880591+JonathanBaker15@users.noreply.github.com>
2021-04-07 09:27:26 -0400

    Update add.php

--- add.php
+++ add.php
@@ -47,23 +47,31 @@ function googleTranslateElementInit() {
 <?php
 
 include "db_connect.php";
-$new_nom = $_GET["newnom"];
-$new_prenom = $_GET["newprenom"];
-//$new_faculte = $_GET["newfaculte"];
-$new_departement = $_GET["newdepartement"];
-$new_nombre_de_cle = $_GET["newnombredecle"];
-$new_code_de_cle = $_GET["newcodedecle"];
-$new_nom_piece = $_GET["newnompiece"];
-$new_batiment = $_GET["newbatiment"];
-$new_date_de_remise = $_GET["newdatederemise"];
-$new_date_de_fin = $_GET["newdatedefin"];
-$new_depot = $_GET["newdepot"];
+$nom = $_GET["nom"];
+$prenom = $_GET["prenom"];
+$num_etu = $_GET["numero_etudiant"];
+$tele = $_GET["tele"];
+$courriel_uottawa = $_GET["courriel_uottawa"];
+$courriel = $_GET["courriel"];
+$contact_urgence = $_GET["contact_urgence"];
+$tele_urgence = $_GET["tele_contact_urgence"];
+$departement = $_GET["departement"];
+$nombre_de_cle = $_GET["nombredecle"];
+$code_de_cle = $_GET["codedecle"];
+$nom_piece = $_GET["nompiece"];
+$batiment = $_GET["batiment"];
+$date_de_remise = $_GET["datederemise"];
+$date_de_fin = $_GET["datedefin"];
+$supmail = $_GET["courriel_sup"];
+$depot = $_GET["depot"];
+$supnom = $_GET["nom_sup"];
+$statue = $_GET["statue"];
+$statue_cle = 'courriel_envoyer';
 
 
-// Search the database for the word bike
 echo "<h1>Vous avez ajouté quelqu'un à la base de données!</h1>";
 
-$sql = "INSERT INTO general (ID, Nom, Prenom, Departement, Nombre_de_cle, Code_de_cle, Nom_piece, Batiment, Date_de_remise, Date_de_fin, Depot) VALUES (NULL, '$new_nom', '$new_prenom', '$new_departement', '$new_nombre_de_cle', '$new_code_de_cle', '$new_nom_piece', '$new_batiment', '$new_date_de_remise', '$new_date_de_fin', '$new_depot')";
+$sql = "INSERT INTO general (ID, Nom, Prenom, Statue, numero_etudiant, courriel_uottawa, telephone, courriel, contact_urgence, telephone_contact_urgence, nom_superviseur, courriel_superviseur, Departement, Nombre_de_cle, Code_de_cle, Nom_piece, Batiment, Date_de_remise, Date_de_fin, Depot, statue_cle) VALUES (NULL, '$nom', '$prenom', '$statue', '$num_etu', '$courriel_uottawa', '$tele', '$courriel', '$contact_urgence', '$tele_urgence', '$supnom', '$supmail', '$departement', '$nombre_de_cle', '$code_de_cle', '$nom_piece', '$batiment', '$date_de_remise', '$date_de_fin', '$depot', '$statue_cle')";
 $result = $mysqli->query($sql);
 
 //include "search.php";
